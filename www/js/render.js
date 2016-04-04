3b352512402b084d2f54e4c688c9a2236aa8e512
Sergey Petukhov <esrb07@mail.ru>
2016-04-04 18:35:03 +0000

    upload

--- www/js/render.js
+++ www/js/render.js
@@ -1,8 +1,40 @@
 $(document).ready(function() {
-    var source   = $("#table").html();
-    var template = Handlebars.compile(source);
+    var url = (window.location.pathname == '/www/pages/property.html') ? 'https://realty-serzh.rhcloud.com/getAllProperties' : 
+                                                                         'https://realty-serzh.rhcloud.com/getAllHire';
     
-    var data = { "address": "Moscow", "description": "qwerty", "fee": "1600" };
-    
-    $("#insertion").append(template(data));
+    handlebarsRenderProperty(url);
 });
+
+
+function executeStateProperty(url) {
+    var xmlhttp;
+
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
+
+
+function handlebarsRenderProperty(url) {
+    var data;
+    
+    $.ajax({
+        async: false,
+        global: false,
+        url: url,
+        dataType: 'json',
+        success: function (d) {
+            data = d;
+        }
+    });
+    
+    var template = Handlebars.compile($("#tableTemplate").html());
+    
+    $(".inserHere").append(template(data));
+}
